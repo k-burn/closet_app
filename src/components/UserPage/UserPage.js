47c7eece927a930184e960be1dea0f88c79e6b0a
k-burn <karaburnett93@gmail.com>
2018-09-22 18:32:05 -0500

    weather api GET works and displays on DOM of user home

--- src/components/UserPage/UserPage.js
+++ src/components/UserPage/UserPage.js
@@ -5,6 +5,7 @@ import Nav from '../../components/Nav/Nav';
 
 import { USER_ACTIONS } from '../../redux/actions/userActions';
 import { triggerLogout } from '../../redux/actions/loginActions';
+import axios from 'axios';
 
 
 const mapStateToProps = state => ({
@@ -12,6 +13,18 @@ const mapStateToProps = state => ({
 });
 
 class UserPage extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+        current_temp: '',
+        humidity: '',
+        sky: '' ,
+        description: '',
+        wind: '',
+    };
+  }
+  
   componentDidMount() {
     this.props.dispatch({ type: USER_ACTIONS.FETCH_USER });
   }
@@ -21,6 +34,29 @@ class UserPage extends Component {
       this.props.history.push('home');
     }
   }
+
+  getWeather = () => {
+    console.log('in getWEATHER');
+    
+    axios({
+        method: 'GET', 
+        url: 'http://api.openweathermap.org/data/2.5/forecast?q=Minneapolis,US&APPID=c0ff50f9018d710e46090ab603c45046&units=imperial'
+      }).then((response) => {
+        console.log(response.data.list[0]);
+        this.setState({
+          current_temp: response.data.list[0].main.temp,
+          humidity: response.data.list[0].main.humidity,
+          sky: response.data.list[0].weather[0].main ,
+          description: response.data.list[0].weather[0].description ,
+          wind: response.data.list[0].wind.speed 
+        })
+
+      }).catch(function(error){
+        console.log('Error in getWeather:', error);
+        alert('Error Getting Weather')
+      });
+  }
+  
 
   logout = () => {
     this.props.dispatch(triggerLogout());
@@ -38,11 +74,26 @@ class UserPage extends Component {
             Welcome, { this.props.user.userName }!
           </h1>
           <p>Your ID is: {this.props.user.id}</p>
+          <div id="weatherInfo">
+            <input placeholder="Current City"></input>
+            <button onClick={this.getWeather}>
+              Get Weather
+            </button>
+            <p>{JSON.stringify(this.state)}</p>
+            <h4>Current Weather</h4>
+            <p>Temperature: {this.state.current_temp} F</p>
+            <p>Humidity: {this.state.humidity} %</p>
+            <p>Conditions: {this.state.sky}</p>
+            <p> {this.state.description}</p>
+            <p>Wind: {this.state.wind} mph</p>
+
+          </div>
           <button
             onClick={this.logout}
           >
             Log Out
           </button>
+        
         </div>
       );
     }
