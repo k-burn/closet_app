df0e2a5c0e06f714292fe441f53fe614d2ea8e69
k-burn <karaburnett93@gmail.com>
2018-09-24 10:49:26 -0500

    added a weatherReducer

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,10 +1,12 @@
 import { combineReducers } from 'redux';
 import user from './userReducer';
 import login from './loginReducer';
+import weatherData from './weatherReducer';
 
 const store = combineReducers({
   user,
   login,
+  weatherData,
 });
 
 export default store;
---
+++ src/redux/reducers/weatherReducer.js
@@ -0,0 +1,9 @@
+import { combineReducers } from 'redux';
+import { USER_ACTIONS } from '../actions/userActions';
+
+const weatherData = (state = {}, action)=>{
+    if (action.type === 'GET_WEATHER'){
+        return  action.payload;
+    }
+    return state;
+}
